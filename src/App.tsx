85ccf41d1a588f97b768f6f74973e0458c2ad297
nilokillian <nilokillian@gmail.com>
2021-08-12 14:27:19 +1200

    import fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
-import { TextField } from "@fluentui/react";
 import React from "react";
 
 import "./App.css";
 import { Form } from "./components/form/Form.component";
-import { Header } from "./components/header/Header.component";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <Form />
     </div>
   );
-}
+};
 
 export default App;
